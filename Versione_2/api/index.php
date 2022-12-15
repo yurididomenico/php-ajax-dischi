3a9f09c64ebdce6856d6c7372808058e4cf3a3bd
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-15 18:12:08 +0100

    Stampo il database in /api per verifica

--- Versione_2/api/index.php
+++ Versione_2/api/index.php
@@ -1,8 +1,10 @@
 <?php
 
-    include_once __DIR__ . '/../db/index.php';
+    include __DIR__ . '/../db/index.php';
 
     // var_dump( __DIR__ );
 
-    
+    header('Content-type: application/json');
+
+    echo json_encode($database);
 ?>
