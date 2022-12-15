7141ad177038583b07b7dcf292fe5ea7d73d5440
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-15 17:45:58 +0100

    Versione 2: Set Workspace

--- Versione_1/index.php
+++ Versione_1/index.php
@@ -83,7 +83,7 @@ $database =
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>YDD - ajax dischi</title>
+    <title>YDD - Ajax Dischi V1</title>
     <!-- [______Collegamento Bootstrap__________________________________________] -->
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
     <!-- [______Collegamento Font__________________________________________] -->
---
+++ Versione_2/assets/js/main.js
@@ -0,0 +1,24 @@
+var app = new Vue
+({
+    el: '#app',
+    data: 
+    {
+
+    },
+
+    mounted()
+    {
+
+    },
+
+    beforeUpdate()
+    {
+
+    },
+
+    methods:
+    {
+
+    }
+
+})
